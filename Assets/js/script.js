dfd0c9e9fb253ba200f63aad364148e6824c9b1d
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-18 14:46:55 -0600

    Added pseudo-code, notes for development

---
+++ Assets/js/script.js
@@ -0,0 +1,4 @@
+// Readme shows a save button on the right, presumably to log the content to local storage.
+// JS will need to be added for the buttons themselves.
+// Timeblocks will need to change color depending on which hour it is/has passed/is upcoming. MomentJS integration.
+// Clock at the top of the page alongside the date might be a nice touch.
