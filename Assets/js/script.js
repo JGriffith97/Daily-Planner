a525cea7854bc15295b8c0539916aac9c85d47c1
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-18 17:05:15 -0600

    Added click event listener and stand-in function to eventually store the textarea-added event to local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,12 @@
-// Readme shows a save button on the right, presumably to log the content to local storage.
+// Readme shows a save button on the right to log the content to local storage.
 // JS will need to be added for the buttons themselves.
 // Timeblocks will need to change color depending on which hour it is/has passed/is upcoming. MomentJS integration.
 // Clock at the top of the page alongside the date might be a nice touch.
+
+var save = document.getElementById("saveBtn");
+var text = document.querySelectorAll("textarea")
+
+$(save).on("click", function() {
+  saveEvent();
+})
+
