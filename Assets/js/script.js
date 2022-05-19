3ef3da1b1e2dd0bc4eb52d80d89753c9f3736a44
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-19 15:57:07 -0600

    Added pseudo-code for class reference

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,11 +9,21 @@ var text = document.querySelectorAll("textarea");
 var currentDateTime = null,
 date = null;
 
+// Tells the time displayed on the page to change based on the readied function
+// belows interval, every half-second. (Half so that it's more accurate)
 function update() {
   date = moment(new Date())
   currentDateTime.html(date.format("dddd, MMMM Do YYYY, h:mm:ss a"))
 }
 
+// TODO: Funtion for changing the color of the timeblock based on the time
+// if time has passed, current time, upcoming time.
+// jQuery .eq method with if, else if statements to determine current time?
+
+
+// TODO: Figure out how to save an individual line, the one associated with the
+// save button, to save that line ONLY to local storage so that it persists when
+// the page is refreshed.
 function saveEvent(event) {
   
 }
@@ -22,7 +32,7 @@ function saveEvent(event) {
 $(document).ready(function() {
   currentDateTime = $("#currentDay")
   update();
-  setInterval(update, 1000);
+  setInterval(update, 500);
 });
 
 $(save).on("click", function(event) {
